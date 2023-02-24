27d3751eb0320e08d49e49d3affc85b2843ddd70
annisaabuhamid <siti.nurannisa@cleargo.com>
2023-02-24 16:31:20 +0800

    1.First Commit

--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+module.exports = {
+  reactStrictMode: true,
+  images: {
+    loader: "default",
+    domains: ["localhost"],
+  },
+};
