c7a2038b66a6c66507b3c5a22457bd6e5736962e
annisaabuhamid <siti.nurannisa@cleargo.com>
2023-03-01 18:29:35 +0800

    1. create news page 2. called article

---
+++ pages/category/news/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+
+import Articles from "../../../components/articles";
+import Layout from "../../../components/layout";
+import Seo from "../../../components/seo";
+import { fetchAPI } from "../../../lib/api";
+
+
+const News = ({ articles, categories, news }) => {
+  return (
+    <Layout categories={categories}>
+      
+      <div className="uk-section">
+        <div className="uk-container uk-container-large">
+          <Articles articles={articles} />
+        </div>
+      </div>
+    </Layout>
+  );
+};
+export async function getStaticProps() {
+  // Run API calls in parallel
+  const [articlesRes, categoriesRes, newsRes] = await Promise.all([
+    fetchAPI("/articles", { populate: ["image", "category"] }),
+    fetchAPI("/categories", { populate: "*" })
+    
+  ]);
+
+  return {
+    props: {
+      articles: articlesRes.data,
+      categories: categoriesRes.data,
+      
+    },
+    revalidate: 1,
+  };
+}
+
+
+export default News;
